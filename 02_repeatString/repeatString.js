2fee4bd87f7d60305c2c59972bbbced2d446b755
Jack Rivellese <186514166+jcrivel@users.noreply.github.com>
2024-10-29 22:15:05 -0400

    Finished Exercises 01,02, and 03

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, numOfTimes) {
+    if(numOfTimes < 0){
+        return "ERROR"
+    }
+    let returnString = ""
+    for(let i = 0; i < numOfTimes; i++){
+        returnString += string
+    }
+    return returnString
 };
 
 // Do not edit below this line
